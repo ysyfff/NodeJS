c7c8e8ea15ee70e6f413c29ab041fc8b134ff37f
ysyong <1156546473@qq.com>
2013-10-19 12:54:09 +0800

    借助fs模块添加了show-img的功能

--- upload/requestHandlers.js
+++ upload/requestHandlers.js
@@ -1,7 +1,8 @@
 var exec = require("child_process").exec;
 var querystring = require('querystring');
+var fs = require('fs');
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -29,8 +30,24 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response, postData){
+  console.log('Request handler show was called.');
+  fs.readFile('/tmp/test.png', 'binary', function(error, file){
+    if(error){
+      response.writeHead(500, {'Content-Type': 'text/plain'});
+      response.write(error, '\n');
+      response.end();
+    }else{
+      response.writeHead(200, {'Content-Type': 'image/png'});
+      response.write(file, 'binary');
+      response.end();
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 
 exports.start = start;
 exports.upload = upload;
--- upload/server.js
+++ upload/server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var url = require('url');
 
-
 function start(route, handle) {
   function onRequest(request, response) {
     var postData = '';
